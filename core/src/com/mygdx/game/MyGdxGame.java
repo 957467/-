259bcbaf1c1843bf8fdfc511b1234bf8f085f89f
kirill <krl.belozerov@gmail.com>
2026-04-17 17:24:02 +0200

    ура

---
+++ core/src/com/mygdx/game/MyGdxGame.java
@@ -0,0 +1,34 @@
+package com.mygdx.game;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+import com.mygdx.game.GameSettings;
+
+import screens.GameScreen;
+
+public class MyGdxGame extends Game {
+	public SpriteBatch batch;
+	public OrthographicCamera camera;
+
+	public GameScreen gameScreen;
+
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		camera = new OrthographicCamera();
+		camera.setToOrtho(false, GameSettings.SCREEN_WIDTH, GameSettings.SCREEN_HEIGHT);
+
+		gameScreen = new GameScreen(this);
+
+		setScreen(gameScreen);
+	}
+
+	@Override
+	public void dispose() {
+		batch.dispose();
+	}
+}
---
+++ core/src/screens/GameScreen.java
@@ -0,0 +1,10 @@
+package screens;
+import com.badlogic.gdx.ScreenAdapter;
+import com.mygdx.game.MyGdxGame;
+    public class GameScreen extends ScreenAdapter {
+        MyGdxGame myGdxGame;
+        public GameScreen(MyGdxGame myGdxGame) {
+            this.myGdxGame = myGdxGame;
+        }
+
+}
